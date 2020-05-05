9e3cba7534d4aab356681abaee1b6bec6dc2fdf2
Nabil Kazerouni <nabilkaz@gmail.com>
2020-05-05 08:36:57 -0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -18,9 +19,39 @@ function App() {
         >
           Learn React
         </a>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/listview">List</Link>
+                </li>
+                <li>
+                  <Link to="/additem">add item</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/listview">
+                <Listview />
+              </Route>
+              <Route path="/additem">
+                <Additem />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
+}
+
+function Listview() {
+  return <h2>This is the list view!</h2>;
+}
+
+function Additem() {
+  return <h2>This is where you'd add an item!</h2>;
 }
 
 export default App;
