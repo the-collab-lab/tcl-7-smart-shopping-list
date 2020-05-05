cfc5aec5d3b9684fdec5847f7485a3d1b13e7d84
Megan McKissack <meganmckissack@gmail.com>
2020-05-05 13:00:31 -0700

    added firestore provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from '@firebase/app';
+import '@firebase/firestore';
+import {FirestoreProvider } from 'react-firestore';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const config = {
+    apiKey: "AIzaSyBOsjQqoNjd1M2nLK7vAmZoyUlh9eY3VQk",
+    projectId: "tcl-7-smart-shopping-list",
+};
+
+firebase.initializeApp(config);
+
+ReactDOM.render(<FirestoreProvider firebase={firebase}><App /></FirestoreProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
