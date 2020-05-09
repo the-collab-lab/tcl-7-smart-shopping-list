79ef7656dc79a5aad71b348c6fc05c7d94a9ac04
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2020-05-09 16:39:29 -0400

    get rid of unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { FirestoreDocument } from 'react-firestore';
 import GetList from './Components/getList.js';
 import AddItem from './Components/addItem.js';
 
