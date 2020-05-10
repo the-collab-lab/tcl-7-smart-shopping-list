b414b4a4c564d0243d56679dd7d8595b0f02c945
Megan McKissack <meganmckissack@gmail.com>
2020-05-10 14:05:35 -0700

    fix merge conflicts with code line differences error

--- src/App.js
+++ src/App.js
@@ -7,6 +7,28 @@ import AddItem from './Components/addItem.js';
 function App() {
   return (
     <div className="App">
+      <Router>
+        <main>
+          <Switch>
+            <Route path="/list-view">
+              <Listview />
+            </Route>
+            <Route path="/add-item">
+              <Additem />
+            </Route>
+          </Switch>
+        </main>
+        <footer>
+          <nav>
+            <NavLink to="/list-view">
+              <img src="/img/list.svg" alt="view list" />
+            </NavLink>
+            <NavLink to="/add-item">
+              <img src="/img/add.svg" alt="add item" />
+            </NavLink>
+          </nav>
+        </footer>
+      </Router>
       <GetList />
       <AddItem />
     </div>
