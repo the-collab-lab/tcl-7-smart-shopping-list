1c29663968734740ee7d311aa51f0cba402cbf20
Megan McKissack <meganmckissack@gmail.com>
2020-05-10 14:20:32 -0700

    testing files again for merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from 'react';
 import './App.css';
-
 import GetList from './Components/getList.js';
 import AddItem from './Components/addItem.js';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <main>
-          <Switch>
-            <Route path="/list-view">
-              <Listview />
-            </Route>
-            <Route path="/add-item">
-              <Additem />
-            </Route>
-          </Switch>
-        </main>
-        <footer>
-          <nav>
-            <NavLink to="/list-view">
-              <img src="/img/list.svg" alt="view list" />
-            </NavLink>
-            <NavLink to="/add-item">
-              <img src="/img/add.svg" alt="add item" />
-            </NavLink>
-          </nav>
-        </footer>
-      </Router>
       <GetList />
       <AddItem />
     </div>
