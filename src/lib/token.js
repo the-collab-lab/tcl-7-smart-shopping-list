86748b8c40b48291e79efad4bee84bccc615e648
Nabil Kazerouni <nabilkaz@gmail.com>
2020-05-13 17:15:20 -0700

    Add get set has local token functionality

--- src/lib/token.js
+++ src/lib/token.js
@@ -6750,4 +6750,21 @@ function getToken() {
   return phrase.join(' ');
 }
 
-export default getToken;
+// added setLocalToken, hasLocalToken, getLocalToken
+
+// Checks the local storage to see if it has a property with the key 'token' returns string value else null
+export function getLocalToken() {
+  return window.localStorage.getItem('token');
+}
+
+// Checks to see if 'token' exists and returns boolean
+export function hasLocalToken() {
+  return !!getLocalToken();
+}
+
+// Sets token in local storage
+export function setLocalToken() {
+  const token = getToken();
+  const myStorage = window.localStorage;
+  myStorage.setItem('token', token);
+}
