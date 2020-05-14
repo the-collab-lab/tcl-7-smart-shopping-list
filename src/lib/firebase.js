bca7afabac1fb41c5c495376da4c4c5269dff32f
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2020-05-14 17:35:34 -0400

    delete initialize firestorter

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,14 +6,15 @@ import 'firebase/firestore';
 // Initalize Firebase.
 // These details will need to be replaced with the project specific env vars at the start of each new cohort.
 var firebaseConfig = {
-  apiKey: "AIzaSyBOsjQqoNjd1M2nLK7vAmZoyUlh9eY3VQk",
-  authDomain: "tcl-7-smart-shopping-list.firebaseapp.com",
-  databaseURL: "https://tcl-7-smart-shopping-list.firebaseio.com",
-  projectId: "tcl-7-smart-shopping-list",
-  storageBucket: "tcl-7-smart-shopping-list.appspot.com",
-  messagingSenderId: "1010438909280",
-  appId: "1:1010438909280:web:c6de43190232119c3fc205"
+  apiKey: 'AIzaSyBOsjQqoNjd1M2nLK7vAmZoyUlh9eY3VQk',
+  authDomain: 'tcl-7-smart-shopping-list.firebaseapp.com',
+  databaseURL: 'https://tcl-7-smart-shopping-list.firebaseio.com',
+  projectId: 'tcl-7-smart-shopping-list',
+  storageBucket: 'tcl-7-smart-shopping-list.appspot.com',
+  messagingSenderId: '1010438909280',
+  appId: '1:1010438909280:web:c6de43190232119c3fc205',
 };
+//initialize firestorter
 
 let fb = firebase.initializeApp(firebaseConfig);
 
