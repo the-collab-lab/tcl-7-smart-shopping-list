87c4ab73c11771d733f56ac2f04f68912b39edb5
Nabil Kazerouni <nabilkaz@gmail.com>
2020-05-13 20:03:39 -0700

    Add commented problem solution for getlist

--- src/Components/getList.js
+++ src/Components/getList.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { FirestoreDocument } from 'react-firestore';
+import getLocalToken from '../lib/token.js';
 
 class GetList extends React.Component {
   render() {
     return (
+      // Can we get the collection?
+      // this refers to a single document not the collection
+      // map over the collection pulling out the path for each one
+      // and displaying it as a firestoredocument component
       <FirestoreDocument
         path="items/tIA3kp24eSWYTNKxA3wl"
         render={({ isLoading, data }) => {
