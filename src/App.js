1ea9034170adc813968b3d966129ead7a02a95c4
Nabil Kazerouni <nabilkaz@gmail.com>
2020-05-13 17:17:02 -0700

    Add signin page + create list that tokenizes user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
+import { hasLocalToken, setLocalToken, getLocalToken } from './lib/token.js';
 import GetList from './Components/getList.js';
 import AddItem from './Components/addItem.js';
-import getToken from './lib/token.js';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   NavLink,
 } from 'react-router-dom';
+import { render } from 'react-dom';
 
 function App() {
   return (
@@ -21,6 +22,9 @@ function App() {
             </Route>
             <Route path="/add-item">
               <ItemForm />
+            </Route>
+            <Route path="/">
+              <Landing />
             </Route>
           </Switch>
         </main>
@@ -39,10 +43,30 @@ function App() {
   );
 }
 
+class Landing extends React.Component {
+  render() {
+    if (hasLocalToken()) {
+      return <GetList />;
+    } else {
+      return <SignIn />;
+    }
+  }
+}
+
+function SignIn() {
+  // uses setLocalToken to set token to local storage when button is clicked
+  return (
+    <div>
+      <h1>Welcome to your smart shopping list!</h1>
+      <p>Tap “Create shopping list” to get started.</p>
+      <a href="./add-item">
+        <button onClick={setLocalToken}>Create shopping list</button>
+      </a>
+    </div>
+  );
+}
+
 function Listview() {
-  const token = getToken();
-  const myStorage = window.localStorage;
-  myStorage.setItem('token', token);
   return <GetList />;
 }
 
