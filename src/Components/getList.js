1a5f8974e4b2cbb23391a51f62a1555aea45faa3
Nabil Kazerouni <nabilkaz@gmail.com>
2020-05-15 07:49:57 -0700

    Remove instructional comment

--- src/Components/getList.js
+++ src/Components/getList.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { FirestoreCollection } from 'react-firestore';
 import { getLocalToken } from '../lib/token.js';
 
-// Can we get the collection?
-// this refers to a single document not the collection
-// map over the collection pulling out the path for each one
-// and displaying it as a firestoredocument component
-
 class GetList extends React.Component {
   render() {
     return (
