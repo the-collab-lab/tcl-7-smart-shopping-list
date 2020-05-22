75d8db177cf8569def4e43f4271e6dc53dc601ca
Nabil Kazerouni <nabilkaz@gmail.com>
2020-05-22 08:36:32 -0700

    'Update handelCheckShareCode'

--- src/Components/shareList.js
+++ src/Components/shareList.js
@@ -1,106 +1,77 @@
 import React from 'react';
 import { fb } from '../lib/firebase';
-//import { FirestoreCollection } from 'react-firestore';
-//import GetCollection from '/getCollection.js';
-//import { hasLocalToken, setLocalToken, getLocalToken } from '../lib/token.js';
-//import { GetCollection } from './hasList.js';
-
+import { setLocalToken } from '../lib/token.js';
 
 class ShareList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { token: '' };
 
-    constructor(props) {
-        super(props);
-        this.state = {shareCode: ''};
-    
-        this.updateInput = this.updateInput.bind(this);
-        //this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateInput = this.updateInput.bind(this);
+  }
+
+  updateInput(event) {
+    this.setState({ token: event.target.value });
+  }
+
+  getExistingList() {
+    const db = fb.firestore();
+    const promise = db.collection(this.state.token).get();
+
+    promise.then(querySnapshot => {
+      //check if value has list
+      if (!querySnapshot.empty) {
+        //if yes, set local token and route to list view
+        setLocalToken(this.state.token);
+        window.location.href = '/list-view';
+      } else {
+        //if no, show alert
+        window.alert('Enter a valid share code and try again');
       }
+    });
+  }
 
-      updateInput(event) {
-        this.setState({shareCode: event.target.value});
-      }
-      
-    async hasList (token) {
-        const db = fb.firestore();
-        //collect share code from input to query matching collection name
-       const collectionReference = await db.collection(this.state.shareCode).get()
-       .then(querySnapshot => {
-        if (querySnapshot.empty) {
-          token = false
-        } else {
-          console.log('here');
-          token = true
-        }
-       }
-    )
-       return token
-    }
+  handleCheckShareCode = e => {
+    e.preventDefault();
+    this.getExistingList();
+  };
 
-    /* hasList = (token) => {
-       const db = fb.firestore();
-       const collectionReference = db.collection(token);
-       collectionReference
-         .get()
-         .then(querySnapshot => {
-             if (querySnapshot.empty) {
-               return false
-             } else {
-               console.log('here');
-               return true
-             }
-            }
-         )
-     }*/
-   
-   handleCheckShareCode = e => {
-        e.preventDefault();
-     //get the value from input 
-     //const token = document.getElementById('share-code').value;
-     //check if value has list
-     //if no, show alert
-     //if yes, set local token and route to list view
-     console.log(this.hasList(this.token));
-   if ( this.hasList(this.token) ) {
-       //e.preventDefault();
-     //route to list view
-     
-     console.log('this worked');
-   } else {
-       e.preventDefault();
-       window.alert('Enter a valid share code and try again');
-   }
- 
-   }   
-
- render() {
-   return (
-  <form className="shadow bg-white pa2" action="./add-item">
-   <h1 className="b f1">Welcome to your smart shopping list!</h1>
-   <p className="f3">Enter your share code below. Then tap "Join shopping list" to get started.</p>
-   <label>
-       Share Code
-       <input
-         type="text"
-         name="shareCode"
-         onChange={this.updateInput}
-         value={this.state.shareCode}
-            />
-     </label>
-   <button
-     onClick={this.handleCheckShareCode}
-     className="bg-green ph2 pv1 white f2 b">
-    Join Shopping List
-   </button>
-   <p className="f5 gray">
-     You can also{' '}
-     <a className="black" href="/">
-       create a new shopping list
-     </a>
-     .
-   </p>
- </form>
-   );
- }
+  render() {
+    return (
+      <form className="shadow bg-white pa2" action="./add-item">
+        <h1 className="b f1">Welcome to your smart shopping list!</h1>
+        <p className="f3">
+          Enter your share code below. Then tap "Join shopping list" to get
+          started.
+        </p>
+        <label>
+          Share Code
+          <input
+            type="text"
+            name="shareCode"
+            onChange={this.updateInput}
+            value={this.state.shareCode}
+          />
+        </label>
+        <p>
+          <button
+            onClick={this.handleCheckShareCode}
+            className="bg-green ph2 pv1 white f2 b"
+          >
+            Join Shopping List
+          </button>
+        </p>
+        <p className="f5 gray">
+          You can also{' '}
+          <a className="black" href="/">
+            {' '}
+            create a new shopping list
+          </a>
+          .
+        </p>
+      </form>
+    );
+  }
 }
- 
- export default ShareList;
+
+export default ShareList;
