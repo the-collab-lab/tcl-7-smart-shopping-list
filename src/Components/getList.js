0046a814c0a2db4a14e51f5221d8902964b1db67
Megan McKissack <meganmckissack@gmail.com>
2020-06-07 09:59:18 -0700

    ran prettier on getList.js file to fix code formatting

--- src/Components/getList.js
+++ src/Components/getList.js
@@ -4,8 +4,6 @@ import { getLocalToken } from '../lib/token.js';
 import { useHistory } from 'react-router-dom';
 import { useState } from 'react';
 
-
-
 function EmptyList() {
   let history = useHistory();
 
@@ -27,72 +25,50 @@ function EmptyList() {
   );
 }
 
-
-const FullList = (props)=> {
+const FullList = (props) => {
   const [isChecked, setCheck] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setSearchTerm(e.target.value);
   };
 
   const clearSearch = () => {
-    setSearchTerm("");
+    setSearchTerm('');
   };
 
-  const searchList = props.data.filter(item => {
+  const searchList = props.data.filter((item) => {
     return item.itemName.toLowerCase().includes(searchTerm.toLowerCase());
   });
-
-
 
   return (
     <div className="grocery-list">
       <h1>Groceries</h1>
       <ul className="tl f2 check-list">
-      <li className="bg-white pa1 shadow">
-      <div className="filter-input">
-      <input 
-         type="text"
-         placeholder="Filter List"
-         value={searchTerm}
-         onChange={handleChange}
-       />
-       <button className="filter-button" onClick={clearSearch}> X </button>
-       </div>
-       </li>
+        <li className="bg-white pa1 shadow">
+          <div className="filter-input">
+            <input
+              type="text"
+              placeholder="Filter List"
+              value={searchTerm}
+              onChange={handleChange}
+            />
+            <button className="filter-button" onClick={clearSearch}>
+              {' '}
+              X{' '}
+            </button>
+          </div>
+        </li>
 
-    
         {searchTerm
-          ? searchList.map(item => (
-            <li className="bg-white pa1 shadow" key={item.id}>
-               <div className="checkbox-input">
-            <label>
-                    <input 
+          ? searchList.map((item) => (
+              <li className="bg-white pa1 shadow" key={item.id}>
+                <div className="checkbox-input">
+                  <label>
+                    <input
                       type="checkbox"
-                        value= {isChecked}
-                        onChange={() => setCheck(checked => !checked)}
-                      defaultChecked={
-                        item.lastPurchasedDate
-                          ? Date.now() / 1000 - item.lastPurchasedDate.seconds <
-                            86400
-                          : false
-                      }
-                    />
-              {item.itemName}
-              </label>
-            </div>
-            </li>
-          ))
-        : props.data
-        .map(item => (
-          <li className="bg-white pa1 shadow" key={item.id}>
-            <div className="checkbox-input">
-            <label>
-                    <input 
-                      type="checkbox"
-                        value= {isChecked}
-                        onChange={() => setCheck(checked => !checked)}
+                      value={isChecked}
+                      onChange={() => setCheck((checked) => !checked)}
                       defaultChecked={
                         item.lastPurchasedDate
                           ? Date.now() / 1000 - item.lastPurchasedDate.seconds <
@@ -101,14 +77,34 @@ const FullList = (props)=> {
                       }
                     />
                     {item.itemName}
-            </label>
-            </div>
-          </li>
-        ))}
+                  </label>
+                </div>
+              </li>
+            ))
+          : props.data.map((item) => (
+              <li className="bg-white pa1 shadow" key={item.id}>
+                <div className="checkbox-input">
+                  <label>
+                    <input
+                      type="checkbox"
+                      value={isChecked}
+                      onChange={() => setCheck((checked) => !checked)}
+                      defaultChecked={
+                        item.lastPurchasedDate
+                          ? Date.now() / 1000 - item.lastPurchasedDate.seconds <
+                            86400
+                          : false
+                      }
+                    />
+                    {item.itemName}
+                  </label>
+                </div>
+              </li>
+            ))}
       </ul>
     </div>
   );
-}
+};
 
 function GetList() {
   return (
