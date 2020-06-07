97fa3ac3d0864866357d6fe790620af7366a780f
Megan McKissack <meganmckissack@gmail.com>
2020-06-07 10:36:01 -0700

    removed unused components that were commented out

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { hasLocalToken, setLocalToken } from './lib/token.js';
 import GetList from './Components/getList.js';
 import AddItem from './Components/addItem.js';
 import ShareList from './Components/shareList.js';
-//import FilterList from './Components/listFilter.js'
+
 import {
   BrowserRouter as Router,
   useHistory,
@@ -87,7 +87,6 @@ function SignIn() {
 }
 
 function Listview() {
-  //return <FilterList />
   return <GetList />;
 }
 
