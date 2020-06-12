6f1a41a69c55c017b1847242efb89ee6ac981db7
Megan McKissack <meganmckissack@gmail.com>
2020-06-11 23:02:13 -0700

    created sorting functionality for list items: alphabetically and by estimate and set up listbackground change on frequency conditions but not connected to list items

--- src/Components/getList.js
+++ src/Components/getList.js
@@ -24,7 +24,18 @@ function EmptyList() {
     </div>
   );
 }
-
+const FrequencyColor = (props) => {
+   if (props.data.purchaseFrequency <= 7) {
+     return '#88c057';
+   } else if (props.data.purchaseFrequencyy <= 14) {
+     return '#eeaf4b';
+   } else if (props.data.purchaseFrequency <=30) {
+     return '#ed3f32';
+   } else {
+     return 'white';
+   }
+ }
+ 
 const FullList = (props) => {
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -60,13 +71,38 @@ const FullList = (props) => {
         </li>
 
         {searchTerm
-           ? searchList.map(item => <GroceryItem item={item} />)
-           : props.data.map(item => <GroceryItem item={item} />)
+           ? searchList.map(item => <GroceryItem key={item.id} item={item} />)
+        : props.data.map(item => <GroceryItem key={item.id} item={item} />)
         }
       </ul>
     </div>
   );
 };
+
+
+function EstimatedDaystoNextPurchase (props) {
+  //lastPurchaseDate to seconds 
+  // estimated next purchase date - is last purhchased date + PurchaseFrequency 
+  // subtract today's date from estimated purchase date
+  const millisecPerDay = 100 * 60 * 60 * 24;
+let estimatedNextPurchaseDate = new Date(props.data.lastPurchasedDate);
+  estimatedNextPurchaseDate.setDate(estimatedNextPurchaseDate.getDate() + props.data.purchaseFrequency);
+  let currentDate = Date.now();
+
+console.log(currentDate);
+}
+
+function sortedItems(props) {
+  props.data.sort((a, b) => {
+    /* sort by estimated number of days until next purchase - 
+    is this stored in a variable from previous issue or is it calculated by nextPurchaseDate - LastPurchaseDate */
+    if(a.estimate === b.estimate) {
+    // sorts alphabetically if estimated days until next purchase are the same
+      return a.itemName > b.itemName ? 1 : -1;
+    } 
+    return a.estimate - b.estimate;
+  }
+  )}
 
 function GroceryItem(props) {
   const [isChecked, setCheck] = useState(false);
