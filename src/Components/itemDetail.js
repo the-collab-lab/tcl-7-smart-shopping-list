85ee5bc5a0f31ca79cb017661b614714f7e20dd3
Nabil Kazerouni <nabilkaz@gmail.com>
2020-06-19 08:18:38 -0700

    Add date formatting

--- src/Components/itemDetail.js
+++ src/Components/itemDetail.js
@@ -3,12 +3,25 @@ import '../App.css';
 import { useParams } from 'react-router-dom';
 import { getLocalToken } from '../lib/token.js';
 import { FirestoreDocument } from 'react-firestore';
+import { Link } from 'react-router-dom';
 
 function ItemDetailPage() {
   let { docId } = useParams();
   // date formater
 
   // calculate days until next purchase
+  const getFormattedDate = date => {
+    const seconds = date.seconds;
+    const year = Math.floor(seconds / 31536000);
+    const nummonths = Math.floor((seconds % 31536000) / 2628000);
+    const numdays = Math.floor(((seconds % 31536000) % 2628000) / 86400);
+
+    const month = nummonths.toString().padStart(2, '0');
+    const day = numdays.toString().padStart(2, '0');
+
+    return month + '/' + day + '/' + year;
+  };
+
   // this is duplicated from getList lines 87-93. Should refactor in seperate issue.
   const today = new Date();
   const getDaysToNextPurchase = item => {
@@ -28,7 +41,9 @@ function ItemDetailPage() {
           <div className="shadow bg-white pa2">
             <h1 className="b f1">{data.itemName}</h1>
             <ul className="f5 gray">
-              <li>Last purchase date: {data.lastPurchaseDate}</li>
+              <li>
+                Last purchase date: {getFormattedDate(data.lastPurchasedDate)}
+              </li>
               <li>
                 Estimated number of days until the next purchase:{' '}
                 {getDaysToNextPurchase(data)}
@@ -38,6 +53,7 @@ function ItemDetailPage() {
                 {data.numberOfPurchases}
               </li>
             </ul>
+            <Link to="/list-view">go back to the list</Link>
           </div>
         );
       }}
