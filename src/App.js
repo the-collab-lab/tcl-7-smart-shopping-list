69806c37e072d7d7162dc179f6828d147380d408
Megan McKissack <meganmckissack@gmail.com>
2020-06-19 13:43:12 -0700

    material ui experiments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from '@material-ui/core/Container';
 import { hasLocalToken, setLocalToken } from './lib/token.js';
 import GetList from './Components/getList.js';
 import AddItem from './Components/addItem.js';
 import ShareList from './Components/shareList.js';
+import Button from '@material-ui/core/Button';
 
 import {
   BrowserRouter as Router,
   useHistory,
   Switch,
   Route,
-  NavLink,
   Link,
 } from 'react-router-dom';
 
@@ -19,32 +20,24 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <main className="bg-blue pa1">
-          <Switch>
-            <Route path="/list-view">
-              <Listview />
-            </Route>
-            <Route path="/add-item">
-              <ItemForm />
-            </Route>
-            <Route path="/join-existing">
-              <ShareList />
-            </Route>
-            <Route path="/">
-              <Landing />
-            </Route>
-          </Switch>
+        <main className="bg-deep-blue pa1">
+          <Container maxWidth="md">
+            <Switch>
+              <Route path="/list-view">
+                <Listview />
+              </Route>
+              <Route path="/add-item">
+                <ItemForm />
+              </Route>
+              <Route path="/join-existing">
+                <ShareList />
+              </Route>
+              <Route path="/">
+                <Landing />
+              </Route>
+            </Switch>
+          </Container>
         </main>
-        <footer>
-          <nav>
-            <NavLink to="/list-view">
-              <img src="/img/list.svg" alt="view list" />
-            </NavLink>
-            <NavLink to="/add-item">
-              <img src="/img/add.svg" alt="add item" />
-            </NavLink>
-          </nav>
-        </footer>
       </Router>
     </div>
   );
@@ -69,20 +62,29 @@ function SignIn() {
   };
 
   return (
-    <form className="shadow bg-white pa2" onSubmit={handleSubmit}>
-      <h1 className="b f1">Welcome to your smart shopping list!</h1>
-      <p className="f3">Tap “Create shopping list” to get started.</p>
-      <button className="bg-green ph2 pv1 white f2 b">
-        Create shopping list
-      </button>
-      <p className="f5 gray">
-        You can also{' '}
-        <Link className="black" to="/join-existing">
-          join an existing shopping list
-        </Link>
-        .
-      </p>
-    </form>
+    <div className="shadow bg-white brd pa2">
+      <form onSubmit={handleSubmit}>
+        <h1 className="deep-blue f1 pa2">
+          Welcome to your smart shopping list!
+        </h1>
+        <p className="deep-blue f3">
+          Tap “Create shopping list” to get started.
+        </p>
+        <button className="bg-teal-green pa2 white f2 b">
+          Create shopping list
+        </button>
+      </form>
+      <p className="f5 gray pa2">You can also </p>
+      <Button
+        component={Link}
+        to="/join-existing"
+        variant="contained"
+        size="large"
+        className="bg-teal-green pa2 white f2 b"
+      >
+        join an existing shopping list
+      </Button>
+    </div>
   );
 }
 
