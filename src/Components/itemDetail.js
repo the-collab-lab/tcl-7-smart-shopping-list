8d30f6ee5d4644e37d449f1a505fae3cc3def9d8
Nabil Kazerouni <nabilkaz@gmail.com>
2020-06-19 07:53:50 -0700

    Add firestoredoc component

--- src/Components/itemDetail.js
+++ src/Components/itemDetail.js
@@ -1,44 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import { useParams } from 'react-router-dom';
-import { fb } from '../lib/firebase';
 import { getLocalToken } from '../lib/token.js';
+import { FirestoreDocument } from 'react-firestore';
 
 function ItemDetailPage() {
   let { docId } = useParams();
-  let item;
-
-  const db = fb.firestore();
-  // test docId is ShD6aFa48RPzE7N9UazC
-  const docRef = db.collection(getLocalToken()).doc(docId);
-  docRef
-    .get()
-    .then(function(doc) {
-      if (doc.exists) {
-        item = doc.data();
-      } else {
-        // doc.data() will be undefined in this case
-        console.log('No such document!');
-      }
-    })
-    .catch(function(error) {
-      console.log('Error getting document:', error);
-    });
 
   return (
-    <div className="shadow bg-white pa2">
-      <h1 className="b f1">{item.itemName}</h1>
-      <ul className="f5 gray">
-        <li>Last purchase date: {item.lastPurchaseDate}</li>
-        <li>
-          Estimated number of days until the next purchase:{' '}
-          {item.purchaseFrequency}
-        </li>
-        <li>
-          Number of times the item has been purchase: {item.numberOfPurchases}
-        </li>
-      </ul>
-    </div>
+    <FirestoreDocument
+      path={`${getLocalToken()}/${docId}`}
+      render={({ isLoading, data }) => {
+        return isLoading ? (
+          <div>loading...</div>
+        ) : (
+          <div className="shadow bg-white pa2">
+            <h1 className="b f1">{data.itemName}</h1>
+            <ul className="f5 gray">
+              <li>Last purchase date: {data.lastPurchaseDate}</li>
+              <li>
+                Estimated number of days until the next purchase:{' '}
+                {data.purchaseFrequency}
+              </li>
+              <li>
+                Number of times the item has been purchase:{' '}
+                {data.numberOfPurchases}
+              </li>
+            </ul>
+          </div>
+        );
+      }}
+    />
   );
 }
 
